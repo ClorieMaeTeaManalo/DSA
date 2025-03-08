ed43c83852f8a5708e627653f9be0f0fdf69d171
ClorieMaeTeaManalo <24-13313@.batstate-u.edu.ph>
2025-03-08 11:47:39 +0800

    Activity 2 part1

---
+++ Manalo_Clorie Mae Tea_Lab2.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+    typedef struct Node{
+        string songName;
+        Node *link;
+}Node;
+//Node sample = new Node;
+//sample->songName = "Heaven Knows by Orange Lemon";
+
+//cout << sample->songName << endl;
+
+Node *createNode(string data){
+    Node *newNode = new Node;
+    newNode->songName = data;
+    newNode->link = NULL;
+    return newNode;
+
+}
+
+void traverse(Node *head){
+    Node *temp = new Node;
+    temp = head;
+
+    cout << " My Plsylist" <<endl;
+    while(temp != NULL){
+        cout << temp->songName<< "->" <<endl;
+        if(temp ->link == NULL){
+            cout << "NULL" <<endl;
+
+        }
+
+        temp = temp->link;
+    }
+}
+    int main(){
+        Node *head = createNode("Sanctuary by Joji");
+        traverse(head);
+    }
+
+Node *insertAtEnd(string data, Node *head){
+    if(head == NULL){
+        Node *newNode = createNode(data);
+        head = newNode;
+        cout << "A new node has been inserted at the end \n" << endl;
+        return head;
+    }
+    Node *temp = new Node;
+    temp = head;
+
+    while(temp->link != NULL){
+        temp = temp->link;
+    }
+
+    Node *newNode = createNode(data);
+    temp->link;
+
+    cout << "A new node has been inserted at the end \n" << endl;
+    return head;
+}
+
+int main(){
+    Node *head = createNode("Sanctuary by Joji");
+
+    head = insertAtEnd("Sunday Morning by Maroon 5", head);
+    traverse(head);
+    }
+
+    return 0;
+
+}
